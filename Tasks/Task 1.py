e419fba1ca67ad399dcf8e39af0afc383bba56d3
Narayaaana11 <narayaaana11@gmail.com>
2025-10-07 22:35:54 +0530

    completed the day 1 with learning variables and datatypes

---
+++ Tasks/Task 1.py
@@ -0,0 +1,25 @@
+name = 'Narayana'
+age = 23
+height = 6.0
+student = True
+
+print(f"Name of the person is {name} he was {age} years old and a height of {height} feet and he is student {student}")
+
+print(type(name), type(age), type(height), type(student))
+
+print(f'In the next 5 years i will be {age+5} years old')
+
+x = "10"
+y = 5
+x=int(x)
+print(x+y)
+
+
+
+
+
+
+
+
+
+
