fa9967b5e0e03cd6885eea8f6a684b109f9999cf
Narayaaana11 <narayaaana11@gmail.com>
2025-10-08 01:56:47 +0530

    Completed tasks from day 1

--- Tasks/Task 1.py
+++ Tasks/Task 1.py
@@ -2,7 +2,6 @@ name = 'Narayana'
 age = 23
 height = 6.0
 student = True
-
 print(f"Name of the person is {name} he was {age} years old and a height of {height} feet and he is student {student}")
 
 print(type(name), type(age), type(height), type(student))
@@ -14,8 +13,21 @@ y = 5
 x=int(x)
 print(x+y)
 
+num = 10
+text = "20"
+print(num+int(text))
 
+q = 3.75
+w = int(q)
+print(f'before {q} after {w}')
 
+is_student = True
+print(is_student)
+print(int(True))
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f'Hello, {name}. You are {age} years old.')
 
 
 
