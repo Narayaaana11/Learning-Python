7a92a6e2322a65b740c69be76bef9d24b7d796a4
Narayaaana11 <narayaaana11@gmail.com>
2025-10-07 22:34:54 +0530

    completed the day 1 with learning variables and datatypes

---
+++ variables.py
@@ -0,0 +1,36 @@
+# This is my first python program
+print('I like Vada')
+print("It's really good")
+
+#sring
+first_name='Narayana'
+last_name='Thota'
+print(f"my name is {first_name} {last_name}")
+
+#integer
+age = 25
+std_count = 36
+print(f'my age is {age}')
+print(f'my standard count is {std_count}')
+
+#float
+price=10.50
+gpa=7.89
+distance = 110
+
+print(f'price of a water bottle is {price} \nmy gpa is {gpa} \nmy home town distance from college is {distance}')
+
+#boolean
+
+is_student = True
+for_sale = False
+
+if is_student:
+    print('I am a student.')
+else:
+    print('I am a teacher.')
+
+
+
+
+
