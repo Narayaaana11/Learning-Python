f69d3c6455b306504a83158d5ea57a840cb5958e
Narayaaana11 <narayaaana11@gmail.com>
2025-10-08 18:27:55 +0530

    Variables

--- variables.py
+++ variables.py
@@ -30,7 +30,43 @@ if is_student:
 else:
     print('I am a teacher.')
 
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0w
 
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
 
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = 'awesome'
+def my_func():
+    x = 'fantastic'
+    print('Python is '+x)
+my_func()
+print('Python is '+x)
+
+x = "awesome"
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
 
 
