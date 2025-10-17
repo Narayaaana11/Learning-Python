0101bfccb3eecb4717d6594912d5fc859670320c
Narayaaana11 <narayaaana11@gmail.com>
2025-10-17 21:14:59 +0530

    Python basics

---
+++ Operators.py
@@ -0,0 +1,23 @@
+friends = 22
+friends += 5 #Addition
+friends -= 5 #Subtraction
+friends *= 2 #Multiplication
+friends /= 2 #Division
+friends %= 2 #Modulus
+friends **= 2 #Power
+print(friends)
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)
+print(result)
+results = abs(y)
+print(results)
+resultss = pow(z, 2)
+print(resultss)
+resultsss = max(x, y, z)
+print(resultsss)
+resultssss = min(x, y, z)
+print(resultssss)
